26ad80adc7f6d4db7e62e1d08749f93655e15eed
P5ArtShow2018 <38104389+P5ArtShow2018@users.noreply.github.com>
2018-05-08 15:13:23 -0400

    Add files via upload

---
+++ sturde space1/sketch.js
@@ -0,0 +1,21 @@
+
+
+function preload(){
+  bg = loadImage("assets/maxresdefault.jpg"); 
+
+  Spacesnd = loadSound ("assets/Spacesnd.wav")
+}
+
+
+function setup() {
+  createCanvas(1920,1080);
+
+  Spacesnd.play();
+}
+
+function draw() {
+  image(bg, 0,0);
+  textSize(36);
+text("What is nothing", mouseX, mouseY);
+fill(0, 102, 153);
+}
